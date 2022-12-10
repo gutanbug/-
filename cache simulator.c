343ea334ba47eeb15fbf85708d9acfed2e073753
HyeonMin Cha <112674303+gutanbug@users.noreply.github.com>
2022-12-11 04:46:12 +0900

    Update cache simulator.c

--- cache simulator.c
+++ cache simulator.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct cache{
-	int tag;
+typedef struct blk *block_ptr;
+typedef struct blk{
 	int valid;
-	int time;
-};
-struct cache *cp;
+	int tag;
+	int period;
+}blk;
+block_ptr bp;
 
 int total, miss;
 int lru_time;
 
-int vict(int set, int assoc){
+int vict(int set, int b_count){
 	int i, t = 0;
 	int min = lru_time+1, index = 0;
 	
-	for(i=0;i<assoc;i++){
-		t = cp[set*assoc+i].time;
+	for(i=0;i<b_count;i++){
+		t = bp[set*b_count+i].period;
 		if(min > t){
 			min = t;
 			index = i;
@@ -25,18 +26,18 @@ int vict(int set, int assoc){
 	return index;
 }
 
-void read_data(int addr, int cache, int block, int assoc){
+void read_data(int addr, int cache, int block, int b_count){
 	int set_count, set, i;
 	int size = 10, victim;
-	struct cache *temp;
+	block_ptr temp;
 	
-	set_count = cache / (block*assoc);
+	set_count = cache / (block*b_count);
 	set = (addr/block) % set_count;
 	
-	for(i=0;i<assoc;i++){
-		temp = &cp[set*assoc + i];
+	for(i=0;i<b_count;i++){
+		temp = &bp[set*b_count + i];
 		if(temp->valid == 1 && temp->tag == (addr/block)/set_count){
-			temp->time = lru_time;
+			temp->period = lru_time;
 			return;
 		}else if(temp->valid == 0){
 			if(i < size)
@@ -45,22 +46,22 @@ void read_data(int addr, int cache, int block, int assoc){
 	}
 	miss++;
 	if(size == 10){
-		victim = vict(set, assoc);
-		temp = &cp[set*assoc + victim];
+		victim = vict(set, b_count);
+		temp = &bp[set*b_count + victim];
 		
 		temp->valid = 1;
-		temp->time = lru_time;
+		temp->period = lru_time;
 		temp->tag = (addr/block) / set_count;
 	}else{
-		temp = &cp[set*assoc+size];
+		temp = &bp[set*b_count+size];
 		
 		temp->valid = 1;
-		temp->time = lru_time;
+		temp->period = lru_time;
 		temp->tag = (addr/block)/set_count;
 	}
 }
 
-void simul(int cache, int block, int assoc){
+void simul(int cache, int block, int b_count){
 	total=0;
 	miss=0;
     int empty, addr;
@@ -69,17 +70,14 @@ void simul(int cache, int block, int assoc){
     char mode;
     FILE *fp = NULL;
 	
-	cp = (struct cache*)calloc(num, sizeof(struct cache));
+	bp = (block_ptr)calloc(num, sizeof(blk));
 
     fp = fopen("gcc.trace", "r");
     while(!feof(fp)){
         fscanf(fp, "%c %x %d\n", &mode, &addr, &empty);
-    	
-		switch(mode){
-			case 'l':
-				read_data(addr, cache, block, assoc);
-				total++;
-				break;
+		if(mode == 'l'){
+			read_data(addr, cache, block, b_count);
+			total++;
 		}
 		lru_time++;
     }
@@ -87,16 +85,16 @@ void simul(int cache, int block, int assoc){
 	
 	printf("\ncache size : %d\n", cache);
 	printf("block size : %d\n", block);
-	printf("associative : %d\n", assoc);
+	printf("associative : %d\n", b_count);
 	printf("======================\n");
 	printf("read miss-rate : %.4lf\n\n", miss_rate);
 	
-	free(cp);
+	free(bp);
     fclose(fp);
 }
 
 int main(){
-	int cache, block, assoc;
+	int cache, block, b_count;
 	int input;
 	
 	while(input != 3){
@@ -108,11 +106,11 @@ int main(){
 			printf("block size(16,64)\n: ");
 			scanf("%d", &block);
 			printf("associative(1,2,4,8)\n: ");
-			scanf("%d", &assoc);
+			scanf("%d", &b_count);
 			printf("\nInput Success !\n\n");
 		}
 		else if(input == 2)
-			simul(cache, block, assoc);
+			simul(cache, block, b_count);
 		else
 			exit(0);
 	}
